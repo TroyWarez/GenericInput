8bcfedbc36b53c2447625bbe6760b74fe5ddf0b5
Troy <TroyWarez@troysdomain.com>
2026-02-12 04:24:03 -0700

    xinput 1_3 export symbols

--- Win32/GenericInput/dllmain.cpp
+++ Win32/GenericInput/dllmain.cpp
@@ -2,7 +2,12 @@
 #include "pch.h"
 #include "Window.h"
 #include "ControllerScanner.h"
+#include <array>
+
 extern Window windowManager;
+
+HMODULE g_xinputModule = nullptr;
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -15,11 +20,28 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     {
     case DLL_PROCESS_ATTACH:
     case DLL_THREAD_ATTACH:
+    {
+        WCHAR path[MAX_PATH] = { '/0' };
+        GetSystemDirectoryW(path, MAX_PATH);
+        g_xinputModule = LoadLibraryW((std::wstring(path) + L"\\XInput1_4.dll").c_str());
+        
+        if (g_xinputModule)
+        {
+            GetProcAddress(g_xinputModule, "XInputGetState");
+			GetProcAddress(g_xinputModule, "XInputSetState");
+        }
+    }
+    break;
     case DLL_THREAD_DETACH:
         break;
     case DLL_PROCESS_DETACH:
         {
         windowManager.UnregisterWindow();
+        if (g_xinputModule)
+        {
+            FreeLibrary(g_xinputModule);
+            g_xinputModule = nullptr;
+		}
         break;
         }
     }
