0e40bd315fe91242b1263c4810d89bdba1027dd9
Troy <TroyWarez@troysdomain.com>
2026-02-20 13:26:31 -0700

    Added a struct for all Xinput dlls (wip). Restored .def file

--- Win32/GenericInput/dllmain.cpp
+++ Win32/GenericInput/dllmain.cpp
@@ -3,19 +3,44 @@
 #include "Window.h"
 #include "ControllerScanner.h"
 #include <array>
+#include <string.h>
 #include <Xinput.h>
+#include "GenericInput.h"
 
-// const std::array<std::string, 4> xinputDlls = {
-//     "XINPUT1_4.dll",
-//     "XINPUT1_3.dll",
-//     "XINPUT9_1_0.dll",
-//     "XINPUT1_2.dll"
-// };
+constexpr size_t NXINPUT_DLL_EXPORTS = 5;
+constexpr size_t NXINPUT_DLL_ORDINALS = 7;
 
-constexpr LPCWSTR XINPUT_DLL = L"\\XInput1_3.dll";
+constexpr size_t NXINPUT_DLLS = 5;
+
+
+struct XInputDll
+{
+    std::array<std::wstring, NXINPUT_DLL_EXPORTS> ExportSymbols;
+    std::array<DWORD, NXINPUT_DLL_ORDINALS>  ExportOrdinals;
+	std::wstring XInputDllBinPath; // Must be load from the system directory to avoid shenanigans
+};
+
+const std::array<XInputDll, NXINPUT_DLLS> XinputDlls = {
+
+    XInputDll{ { L"XInputGetCapabilities", L"XInputEnable", L"XInputGetBatteryInformation", L"XInputGetKeystroke", L"XInputGetAudioDeviceIds" }, 
+    { 10, 100, 101, 102, 103, 104, 108 }, L"\\XInput1_4.dll" },
+
+    XInputDll{ { L"XInputGetCapabilities", L"XInputEnable", L"XInputGetDSoundAudioDeviceGuids", L"XInputGetBatteryInformation", L"XInputGetKeystroke" },
+    { 100, 101, 102, 103, 0, 0 }, L"\\XInput1_3.dll" },
+
+    XInputDll{ { L"XInputGetCapabilities", L"XInputGetDSoundAudioDeviceGuids", L"", L"", L"" },
+    { 0, 0, 0, 0, 0, 0, 0 }, L"\\XInput1_2.dll" },
+
+	XInputDll{ { L"XInputGetCapabilities", L"XInputGetDSoundAudioDeviceGuids", L"", L"", L"" },
+	{ 0, 0, 0, 0, 0, 0, 0 }, L"\\XInput1_1.dll" },
+
+    XInputDll{ { L"XInputGetCapabilities", L"XInputEnable", L"XInputGetBatteryInformation", L"XInputGetDSoundAudioDeviceGuids",   L"XInputGetKeystroke" }, 
+    { 0, 0, 0, 0, 0, 0, 0 }, L"\\XInput9_1_0.dll" }
+};
+
 extern Window windowManager;
 
-HMODULE g_xinputModule = nullptr;
+HMODULE g_hXinputModule = nullptr;
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -28,16 +53,13 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
+	case DLL_THREAD_ATTACH: // Hook Window procedure to detect controller changes and scan for controllers when a new window is created
     {
-        if (g_xinputModule == nullptr)
+        if (g_hXinputModule == nullptr)
         {
-			std::array<WCHAR, MAX_PATH>  path = { L'\0' };
-			GetSystemDirectoryW(path.data(), MAX_PATH);
-			g_xinputModule = LoadLibraryW((std::wstring(path.data()) + XINPUT_DLL).c_str());
-			if (!g_xinputModule)
+			if (!g_hXinputModule)
 			{
-				MessageBoxW(nullptr, L"Failed to load XInput1_3.dll. Please ensure it is present in the system directory.", L"Error", MB_ICONERROR);
+				MessageBoxW(nullptr, std::wstring(L"Failed to load" XINPUT_DLL ".Please ensure it is present in the system directory.").c_str(), L"Error", MB_ICONERROR);
 				return 1;
 			}
         }
@@ -48,10 +70,10 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     case DLL_PROCESS_DETACH:
         {
         windowManager.UnregisterWindow();
-        if (g_xinputModule)
+        if (g_hXinputModule)
         {
-            FreeLibrary(g_xinputModule);
-            g_xinputModule = nullptr;
+            FreeLibrary(g_hXinputModule);
+            g_hXinputModule = nullptr;
 		}
         break;
         }
